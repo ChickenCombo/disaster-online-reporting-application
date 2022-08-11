543da191cbff78100a40a6e1d5531c22bbc264c6
Lester Cuadra <johnlestercuadra.dev@gmail.com>
2022-08-11 20:37:31 +0800

    Added "Jump to Top" button on ReportsFragment

--- app/src/main/java/com/app/dorav4/fragments/ReportsFragment.java
+++ app/src/main/java/com/app/dorav4/fragments/ReportsFragment.java
@@ -1,17 +1,26 @@
 package com.app.dorav4.fragments;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.app.dorav4.R;
 import com.app.dorav4.activities.PostReportActivity;
@@ -34,6 +43,8 @@ public class ReportsFragment extends Fragment {
     RecyclerView recyclerView;
     List<Reports> reportsList;
     ReportsAdapter reportsAdapter;
+    NestedScrollView nestedScrollView;
+    Button btnJumpToTop;
 
     DatabaseReference reportsReference;
 
@@ -48,7 +59,9 @@ public class ReportsFragment extends Fragment {
 
         emptyView = view.findViewById(R.id.emptyView);
         addReport = view.findViewById(R.id.addReport);
+        btnJumpToTop = view.findViewById(R.id.btnJumpToTop);
         recyclerView = view.findViewById(R.id.recyclerView);
+        nestedScrollView = view.findViewById(R.id.nestedScrollView);
 
         reportsReference = FirebaseDatabase.getInstance().getReference().child("Reports");
 
@@ -56,6 +69,26 @@ public class ReportsFragment extends Fragment {
         addReport.setOnClickListener(v -> {
             intent = new Intent(getActivity(), PostReportActivity.class);
             startActivity(intent);
+        });
+
+        // btnJumpToTop OnClickListener
+        btnJumpToTop.setOnClickListener(v -> {
+            nestedScrollView.smoothScrollTo(0, 0);
+        });
+
+        // Show btnJumpToTop button at the end of nested scroll view
+        nestedScrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                if (nestedScrollView.getScrollY() == 0) {
+                    btnJumpToTop.setVisibility(View.GONE);
+                } else if (nestedScrollView.getChildAt(0).getBottom() <= (nestedScrollView.getHeight() + nestedScrollView.getScrollY())) {
+                    btnJumpToTop.animate().alpha(1.0f);
+                    btnJumpToTop.setVisibility(View.VISIBLE);
+                } else {
+                    btnJumpToTop.setVisibility(View.GONE);
+                }
+            }
         });
 
         loadReports();
