b13c609554dc3c38db332e032aff41e853372227
Lester Cuadra <lester.cuadra24@gmail.com>
2022-09-07 20:55:10 +0800

    Added receiver's user info in chat UI

--- app/src/main/java/com/app/dorav4/activities/ConversationActivity.java
+++ app/src/main/java/com/app/dorav4/activities/ConversationActivity.java
@@ -1,5 +1,6 @@
 package com.app.dorav4.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,31 +12,82 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.app.dorav4.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 public class ConversationActivity extends AppCompatActivity {
     TextView tvToolbarHeader;
-    ImageView ivBack, ivSend;
+    ImageView ivBack, ivSend, ivReceiverPicture;
     RecyclerView recyclerView;
     EditText etChat;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    DatabaseReference usersReference;
+
+    String receiverUserId;
+    String currentUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
 
-        Intent intent = getIntent();
-        String uid = intent.getStringExtra("userId");
-
         tvToolbarHeader = findViewById(R.id.tvToolbarHeader);
         etChat = findViewById(R.id.etChat);
         ivBack = findViewById(R.id.ivBack);
         ivSend = findViewById(R.id.ivSend);
+        ivReceiverPicture = findViewById(R.id.ivReceiverPicture);
         recyclerView = findViewById(R.id.recyclerView);
 
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        // Get uid
+        Intent intent = getIntent();
+        receiverUserId = intent.getStringExtra("userId");
+        currentUserId = mUser.getUid();
+
+        usersReference = FirebaseDatabase.getInstance().getReference("Users");
+
+        // Fetch receiver's information
+        getUserInfo(receiverUserId);
+
         // ivSend OnClickListener
-        ivSend.setOnClickListener(v -> Toast.makeText(this, uid, Toast.LENGTH_SHORT).show());
+        ivSend.setOnClickListener(v -> {});
 
         // ivBack OnClickListener
         ivBack.setOnClickListener(v -> finish());
+    }
+
+    // Fetch receiver's information
+    private void getUserInfo(String receiverUserId) {
+        usersReference.orderByKey().equalTo(receiverUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds: snapshot.getChildren()) {
+                    // Fetch data from the database
+                    String receiverFullName = Objects.requireNonNull(ds.child("fullName").getValue()).toString();
+                    String receiverProfilePicture = Objects.requireNonNull(ds.child("profilePicture").getValue()).toString();
+
+                    // Set data
+                    tvToolbarHeader.setText(receiverFullName);
+                    Picasso.get().load(receiverProfilePicture).into(ivReceiverPicture);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
