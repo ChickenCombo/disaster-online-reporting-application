58dd8f7935ba8902e1a6362f8eb196124b95439e
Lester Cuadra <lester.cuadra24@gmail.com>
2022-11-26 23:38:53 +0800

    Fixed onboarding activity not showing

--- app/src/main/java/com/app/dorav4/activities/SplashActivity.java
+++ app/src/main/java/com/app/dorav4/activities/SplashActivity.java
@@ -102,7 +102,7 @@ public class SplashActivity extends AppCompatActivity {
 
                         // Redirect to OnboardingActivity if there is no user session
                         // or it is the user's first time
-                        intent = new Intent(SplashActivity.this, LoginActivity.class);
+                        intent = new Intent(SplashActivity.this, OnboardingActivity.class);
                     } else {
                         // Redirect to LoginActivity if there is a user session
                         // or it is not the user's first time
